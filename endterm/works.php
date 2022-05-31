af64b06185213b161aa5f5137a8351542d4a3dcf
KyleGabales <101041623+KyleGabales@users.noreply.github.com>
2022-05-31 12:00:06 +0800

    Add files via upload

---
+++ endterm/works.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WORKS</title>
+	<link rel="stylesheet" type="text/css" href="css/works.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Rubik:wght@500&family=Work+Sans:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+		<div id="Learning-tasks">
+			<div id="LT">
+				<div class="repo-title"><h1>LEARNING TASKS</h1></div>
+				<div class="row-1">
+						<div class="effect">
+							<a href="LT/LT2/LT2.php"><img src="LT2.jpg" class="image"></a>
+							<div class="overlay overlayFade">
+								<div class="txt">Learning Task 2</div>
+							</div>
+						</div>
+						<div class="effect">
+							<a href="LT/LT3/LT3.php"><img src="LT3.jpg" class="image"></a>
+							<div class="overlay overlayFade">
+								<div class="txt">Learning Task 3</div>
+							</div>
+						</div>
+						<div class="effect">
+							<a href="LT/LT4/LT4.php"><img src="LT4.jpg" class="image"></a>
+							<div class="overlay overlayFade">
+								<div class="txt">Learning Task 4</div>
+							</div>
+						</div>
+					</div>
+				<div class="row-2">
+					<div class="effect2">
+							<a href="LT/CLE1/index.php"><img src="CLE1MT.jpg" class="image2"></a>
+							<div class="overlay2 overlayFade2">
+								<div class="txt">Culminating Learning Evidence 1</div>
+							</div>
+						</div>
+						</div>
+				</div>
+			</div>
+			
+</body>
+</html>
